993adf9e2b13ecc06d43fb9e21f9f6fb8623538b
Dryzin <endrioemanuel11@gmail.com>
2022-07-01 16:53:29 -0300

    Introdução a JS

---
+++ JavaScript/cons.js
@@ -0,0 +1,17 @@
+//console.log("Hello Word!")
+
+function returnEvenValues(array) {
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++){
+        if (array[i] % 2 === 0) {
+            evenNums.push(array[i]); 
+        } else{
+            console.log(`${array[i]} não é par!`);
+        }
+    }
+    console.log("Números pares: ",evenNums);
+}
+
+let array = [1, 2, 3, 4, 8]
+
+returnEvenValues(array);
---
+++ JavaScript/teste.js
@@ -0,0 +1,22 @@
+// Comentar uma linha
+
+/* Comentar
+mais de uma linha */
+
+// Contante sempre começar maiúscula
+
+var desconto = 0.2;
+
+const PRECO = 2;
+
+var total = PRECO - desconto
+
+// Funções
+
+function soma(a, b) {
+    return a + b;//8
+}
+
+//fazer executar
+
+soma(3, 5);
